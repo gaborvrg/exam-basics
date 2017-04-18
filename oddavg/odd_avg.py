afff43d831b835c0958e958c11f6e227387e11f0
Gabor V <gabor.w.varga@gmail.com>
2017-04-18 11:55:52 +0200

    added test unit in odd_avg_test.py

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -4,20 +4,25 @@
 
 class Average(object):
 
-    def odd_average(self, numbers_of_list):
+    def odd_average(self, numbers_of_list = []):
 
         nums_of_odd_numbers = 0
         sum_of_odd_numbers = 0
+        if len(numbers_of_list) == 0 :
+                return print("Cannot division by zero")
+        elif len(numbers_of_list) == 1 and numbers_of_list[0] % 2 == 0:
+            return print("Cannot count average on one even number")
 
-        for num in range(len(numbers_of_list)):
-            if numbers_of_list[num] % 2 == 0:   # even number
-                pass
-            else:   # odd number
-                sum_of_odd_numbers += numbers_of_list[num]
-                nums_of_odd_numbers += 1
+        else:
+            for num in range(len(numbers_of_list)):
+                if numbers_of_list[num] % 2 == 0:   # even number
+                    pass
+                else:   # odd number
+                    sum_of_odd_numbers += numbers_of_list[num]
+                    nums_of_odd_numbers += 1
 
         return sum_of_odd_numbers / nums_of_odd_numbers
-        
+
 
 average = Average()
-print(average.odd_average([1,9,3,5,8]))
+print(average.odd_average([1,2,3]))
--- oddavg/odd_avg_test.py
+++ oddavg/odd_avg_test.py
@@ -1,21 +1,24 @@
 import unittest
-import odd_avg
+from odd_avg import Average
+
+class TestAverage(unittest.TestCase):
 
 
-def test_get_apple(self):
-    apple = Apple()
-    apple.get_apple()
-    self.assertEqual(apple.get_apple(),'apple')
+    def test_odd_average_one_number(self):
+        average = Average()
+        average.odd_average()
+        self.assertEqual(average.odd_average([3]), 3)
 
 
-def test_sum(self):
-    summary = Apple()
-    self.assertEqual(summary.sum([1,2,3]), 6)
+    def test_odd_average_empty(self):
+        average = Average()
+        self.assertEqual(average.odd_average([]), None)
 
-def test_sum_empty(self):
-    summary = Apple()
-    self.assertEqual(summary.sum([]), 0)
+    def test_odd_average(self):
+        average = Average()
+        self.assertEqual(average.odd_average([1,2,3]),2.0)
 
-def test_sum_one_element(self):
-    summary = Apple()
-    self.assertEqual(summary.sum([2]), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
