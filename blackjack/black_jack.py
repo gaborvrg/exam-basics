08e44999be365fc85d7ce5d454158e48ba45862c
Gabor V <gabor.w.varga@gmail.com>
2017-04-18 12:29:10 +0200

    added methods

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -3,18 +3,26 @@
 # Card should be represented as string in this format:
 # 9 Hearts
 # Jack Diamonds
+
+
 # Create a Deck class, that has a list of cards in it
 # Create a constructor that takes a whole number as parameter
-# The constructor should fill the list with the number of different cards using at least 4 different colors (except if the number given is smaller than four, than all cards should have different colors)
+
+# The constructor should fill the list with the number of different cards using 
+# at least 4 different colors (except if the number given is smaller than four, than all cards should have different colors)
+
 # We should be able to shuffle the deck, which randomly orders the cards
 # We should be able to draw the top card which returns the drawn card and also removes it from the deck
 # Deck should be represented as string in this format:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
+
+                # deck = Deck(12)
+                # print(deck)
 # Should print out:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
-top_card = deck.draw()
-print(top_card)
-print(deck)
+                # top_card = deck.draw()
+                # print(top_card)
+                # print(deck)
 # Should print out:
 # Queen Spades
 # 11 cards - 3 Clubs, 3 Diamonds, 3 Hearts, 2 Spades
@@ -22,8 +30,7 @@ print(deck)
 
 class Card(object):
     """docstring for Card"""
-    def __init__(self, color = '', value = ''):
-        # super(Card, self).__init__()
+    def __init__(self, color = ['Hearts','Diamonds', 'Clubs', 'Spades'], value = '3'):
         self.color = color
         self.value = value
 
@@ -33,10 +40,20 @@ class Deck(object):
     list_of_cards = []
 
     def __init__(self, number = 0):
-        # super(Deck, self).__init__()
-        self.arg = arg
+        self.number = number
+
+
+    def shuffle(self):
+        pass
+
+    def draw(self):
+        pass
+
+deck = Deck(12)
+print(deck)
+
+
+
 
 
         
-deck = Deck(12)
-print(deck)
