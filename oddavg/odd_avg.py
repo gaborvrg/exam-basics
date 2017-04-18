1f5281770ad0a645dd3b57621c5ed75255b26534
Gabor V <gabor.w.varga@gmail.com>
2017-04-18 09:34:08 +0200

    added funtcion

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,16 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+def odd_average(numbers_of_list):
+
+    for num in range(len(numbers_of_list)):
+         
+        if numbers_of_list[num] % 2 == 0:   # even number
+            pass
+        else:   # odd number
+            print(numbers_of_list[num])
+
+
+
+odd_average([1,21,3,4,8])
